e2425b458ec203d06b932fb587de1175573d38f9
Faiza Kashaf <faizakashaf4@gmail.com>
2024-01-28 23:22:30 +0500

    added download functionality to each image

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ else{
     // console.log(results)
 
     if(results.length === 0){
-        console.log("hi")
+        // console.log("image not found")
         showMore.innerText = "No image Found";
         showMore.style.display = "block";
         }
@@ -33,28 +33,53 @@ else{
             const images = document.createElement("div");
             images.classList.add ("images");
             const img = document.createElement("img");
+            const icon = document.createElement("i");
+            icon.classList.add('fa','fa-download','image_download');
+            icon.setAttribute("aria-hidden","true");
             img.src = result.urls.small;
             img.alt = result.description;
             images.appendChild(img);
+            images.appendChild(icon);
             wrapper.appendChild(images);
              })
             
              pagecount++
+
+             let all_Icons = document.querySelectorAll(".image_download");
+    all_Icons.forEach((download) => {
+download.addEventListener("click",async (e) => {
+    try{
+        let imgUrl = e.target.parentNode.querySelector("img").src;
+       console.log(imgUrl)
+        let response = await fetch (imgUrl);
+        // console.log(response)
+        let file =await response.blob();
+        // console.log(file)
+        // console.log(file.type)
+        // console.log(file.size)
+        let link = document.createElement("a");
+        link.href = URL.createObjectURL(file);
+        link.download = `${input.value}`;
+        link.click();
+    }catch(error){
+        alert("image not found");
+    }
+})
+    })
             
-            if(pagecount > 1){
+      if(pagecount > 1){
         showMore.innerText = "Show More";
                 showMore.style.display = "block";
-            }}
+     }}
     }
    
 }
 
-form.addEventListener("submit",(e) =>{
+form.addEventListener("submit",async (e) =>{
     wrapper.innerHTML = "";
     e.preventDefault()
-    search_images()
     page = 1; // reset page count to 1 for new search
-      
+ await  search_images()
 })
 showMore.addEventListener("click",()=>{
     search_images()
